be5995ec332df36916db4b58142d9e2f44542a3a
André Luiz Vicente Silva <andre.silva@estudante.iftm.edu.br>
2025-06-09 18:55:08 -0300

    feat: add verboso

--- run_tests.sh
+++ run_tests.sh
@@ -6,7 +6,7 @@ export DATABASE_URL="sqlite:///test.db"
 export PYTHONPATH=$(pwd)
 
 # Executa os testes com pytest
-pytest --maxfail=1 --disable-warnings
+pytest -vv --maxfail=1 --disable-warnings
 
 # Limpa a variável de ambiente após os testes
 unset DATABASE_URL
